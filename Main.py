742de03b056dbdfdb1935281c5cc406f9761be2f
kellensabhlok <78669415+kellensabhlok@users.noreply.github.com>
2022-05-01 14:21:44 -0700

    Created Network Logger
    Updated Main File Too

--- Main.py
+++ Main.py
@@ -5,48 +5,50 @@ import smtplib
 import time
 from datetime import datetime
 from emailScheduler import emailScheduler 
-ps = nmap.PortScanner()
-#Scans a range of hosts ip addresses between 192.168.0.1 and 192.168.0.10 (example ip, we can change this) for multiple devices
-ps.scan(hosts = '192.168.0.1-10', '22')
 
-# clear screen
-sys.stdout.write(u"\u001b[2J\u001b[0;0H")
-sys.stdout.flush()
-time.sleep(0.2)
 
 # iterate over the hosts on the network
 
 class networkMonitor():
 
-    emailScheduler = emailScheduler(7)
-    deviceID, macAddr = initializeNetwork()
+    emailScheduler = emailScheduler(frequencyInDays=7, emailList=["kellens@zoho.com"])
+    # deviceID, macAddr = initializeNetwork()
     
 
     def initializeNetwork(self):
+        ps = nmap.PortScanner()
+        port_list = '21,22,23,80,81,8080'
+#Scans a range of hosts ip addresses between 192.168.0.1 and 192.168.0.10 (example ip, we can change this) for multiple devices
+        ps.scan('192.168.0.1-10', port_list)
 
+# clear screen
+        sys.stdout.write(u"\u001b[2J\u001b[0;0H")
+        sys.stdout.flush()
+        time.sleep(0.2)
         mac = ""
+        print(ps.all_hosts())
         for host in sorted(ps.all_hosts()):
 
             # blank if not Pi
             device = ""
+            
+            # if len(ps[host]['device']) > 0:
 
-            if len(ps[host]['device']) > 0:
+            #     # attempt to get mac address
+            #     try:
+            #         mac = ps[host]['addresses']['mac']
+            #     except:
+            #         mac = ""
 
-                # attempt to get mac address
-                try:
-                    mac = ps[host]['addresses']['mac']
-                except:
-                    mac = ""
-
-                device = tuple(ps[host]['device'].values())
-                if str(device).find('Raspberry') > 0:
-                    device = "Pi!"
-                    # Sets color to white, results hostname, Ip, and Mac Addresses
-                    print(u"\u001b[37m" + "{} ({}) {}\u001b[0m".format(ps[host].hostname(), host, mac))
-                    return device, mac
-                else:
-                    device = ""
-                    print(u"\u001b[0m" + "{} ({}) {}\u001b[0m".format(ps[host].hostname(), host, mac))
+            #     device = tuple(ps[host]['device'].values())
+            #     if str(device).find('Raspberry') > 0:
+            #         device = "Pi!"
+            #         # Sets color to white, results hostname, Ip, and Mac Addresses
+            #         print(u"\u001b[37m" + "{} ({}) {}\u001b[0m".format(ps[host].hostname(), host, mac))
+            #         return device, mac
+            #     else:
+            #         device = ""
+            #         print(u"\u001b[0m" + "{} ({}) {}\u001b[0m".format(ps[host].hostname(), host, mac))
 
     
    
---
+++ network_logger.py
@@ -0,0 +1,12 @@
+from datetime import datetime as dt
+import sys
+from Main import networkMonitor
+
+date = dt.now().strftime("%m-%d-"+"20"+"%y")
+pathname = "Network_Monitor_Logs/"
+filename = date + "-Network_Monitor_Log"
+print(filename)
+
+with open(pathname+filename+".txt", 'w') as output:
+    nm = networkMonitor()
+    nm.initializeNetwork()
